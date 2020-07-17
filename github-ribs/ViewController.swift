612b1cea89ace6adfe4789555893a7dfc32c517f
hslee <hs.lee@cardoc.co.kr>
2020-07-17 11:44:27 +0900

    Initial Commit

---
+++ github-ribs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  github-ribs
+//
+//  Created by HS Lee on 2020/07/17.
+//  Copyright © 2020 HS Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
